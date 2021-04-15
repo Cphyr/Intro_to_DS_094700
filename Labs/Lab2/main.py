1cd0d9ce9c8021d94a16dab18aeea4a50e181b4c
Cphyr <cfir.hadar@gmail.com>
2021-04-15 19:39:35 +0300

    Finito and pep8

--- Labs/Lab2/main.py
+++ Labs/Lab2/main.py
@@ -16,20 +16,93 @@ def load_data(path):
         for row in reader:
             if not read_header:
                 # we are at first row with names of columns
-                for i, column_name in enumerate(row):  # enumerate generates index together with value
+                # enumerate generates index together with value
+                for i, column_name in enumerate(row):
                     data[column_name] = []  # initializing as empty list
                     index_to_column_name[i] = column_name
                 read_header = True
             else:
                 # need to append values to data lists. We don't know column name, only index.
                 for i, value in enumerate(row):
-                    current_column_name = index_to_column_name[i]  # reproducing column name
+                    # reproducing column name
+                    current_column_name = index_to_column_name[i]
                     data[current_column_name].append(float(value))
     return data
 
 
+def get_sorted_tuple(tup):
+    """
+    :arg:
+        :param tup: contains sortable objects
+        :type tup: Tuple()
+
+    :return:
+        A new sorted tuple.
+    """
+    return tuple(sorted(tup))
+
+
+def abs(x):
+    """
+    :arg:
+        :param x:
+        :type x: Number()
+
+    :return:
+        absolute value of x
+    """
+    if(x >= 0):
+        return x
+    return -x
+
+
+def compute_correl(data):
+    """
+    :arg:
+        :param data: data
+        :type data: Dict(Str(), Iterbale(Number()))
+
+    :return:
+        Computes the correlation between every pair of features.
+        Appends the pair of features and the correlations to a dictionary when the
+        pair are the key and the correlation is the value.
+
+        min_pair_key - The name of the pair with the lowest correlation
+        couple_correl_dic[min_pair_key] - The correlation of the weekest_pair
+        max_pair_key - The name of the pair with the strongest correlation
+        couple_correl_dic[max_pair_key] - The correlation of the strongest_pair
+    """
+    # Creates a list of all the pairs of features
+    keys = list(data.keys())
+    couples = [get_sorted_tuple((a, b)) for i, a in enumerate(keys)
+               for b in keys[i + 1:]]
+
+    # Creates the dictionary 'couple_correl_dic'
+    # Adds the couples of features as keys and their
+    # correlation as value
+    couple_correl_dic = {}
+    for couple in couples:
+        first, second = couple
+        couple_correl_dic[couple] = correlation(data[first], data[second])
+
+    # Loops through the dictionary 'couple_correl_dic'
+    # and finds the pairs with the biggest and smallest correlation
+    items = list(couple_correl_dic.items())
+    min_pair_key = items[0][0]
+    max_pair_key = items[0][0]
+    for item in sorted(items):
+        if (abs(item[1]) > abs(couple_correl_dic[max_pair_key])):
+            max_pair_key = item[0]
+        if (abs(item[1]) < abs(couple_correl_dic[min_pair_key])):
+            min_pair_key = item[0]
+
+    # Returns the couple of features with the highest correlation and its correlation
+    # and the couple of features with the lowest correlation and its correlation
+    return min_pair_key, couple_correl_dic[min_pair_key], max_pair_key, couple_correl_dic[max_pair_key]
+
+
 def run_analysis():
-    file_path = './winequality.csv'
+    file_path = './winequality_2.csv'
     data = load_data(file_path)
 
     # first way of printing. Everything casted to string, and spaces put automatically between passed values.
@@ -38,19 +111,17 @@ def run_analysis():
         # second way of printing. We print single string after format function.
         # Format function fills {} with values passed as arguments. It has nice applications for better printing,
         # like limiting number of digits for floats or other formatting tools.
-        print('"{}". Mean: {:3.2f}, Median: {}, Std: {}'.format(
+        print('"{}". Mean: {:3.2f}, Median: {:3.2f}, Std: {:3.4f}'.format(
             feature_name, mean(list_of_values), median(list_of_values), variance(list_of_values)**0.5))
 
     # here you should compute correlations. Be careful, pair should be sorted before printing
-    strongest_pair = ("aaa", "bbb")
-    high_correlation = -0.9
+    weakest_pair, low_correlation, strongest_pair, high_correlation = compute_correl(
+        data)
     print('The strongest linear relationship is between: "{}","{}". '
-          'The value is: {}'.format(strongest_pair[0], strongest_pair[1], high_correlation))
+          'The value is: {:3.4f}'.format(strongest_pair[0], strongest_pair[1], high_correlation))
 
-    weakest_pair = ("aaa", "bbb")
-    low_correlation = 0.1
     print('The weakest linear relationship is between: "{}","{}". '
-          'The value is: {}'.format(*weakest_pair, low_correlation))  # * converts list to arguments.
+          'The value is: {:3.4f}'.format(*weakest_pair, low_correlation))  # * converts list to arguments.
     # Line 53 is equivalent to line 48, this is just other way to use list as arguments
 
 
--- Labs/Lab2/statistics.py
+++ Labs/Lab2/statistics.py
@@ -1,9 +1,10 @@
 def median(list_of_values):
     sorted_list = sorted(list_of_values)
-    center_index = int(len(list_of_values)/2) # round to int required because division always produces float
+    # round to int required because division always produces float
+    center_index = int(len(list_of_values)/2)
 
     # Median value depends on length on list
-    if len(list_of_values)%2 == 0:
+    if len(list_of_values) % 2 == 0:
         result = (sorted_list[center_index] + sorted_list[center_index-1])/2
     else:
         # Now we need only 1 index for exact value
@@ -18,17 +19,90 @@ def mean(list_of_values):
 def variance(list_of_values):
     average = mean(list_of_values)
     squared_sum = sum([(x - average)**2 for x in list_of_values])
-    return squared_sum/(len(list_of_values)-1)
+    return squared_sum / (len(list_of_values) - 1)
+
+
+def standard_deviation(list_of_values):
+    """
+    :arg:
+        :param list_of_values: the first list of data
+        :type list_of_values: List()
+
+    :return:
+        the standard deviation as defined
+    """
+    return variance(list_of_values)**0.5
+
+
+def sub_num_from_iterable(num, iterable):
+    """
+    :arg:
+        :param num: the number to subtract
+        :type num: Number()
+        :param iterable: an iterable to subtract from
+        :type iterable: Iterable()
+
+    :return:
+        returns a new list that contains the elements of :iterable:
+        each subtracted by :num:
+    """
+    return list(map(lambda x: x - num, iterable))
 
 
 def covariance(first_list_of_values, second_list_of_values):
-    result = 0
-    # Place your code here
-    return result
+    """
+    :arg:
+        :param first_list_of_values: the first list of data
+        :type first_list_of_values: List()
+        :param second_list_of_values: the second list of data
+        :type second_list_of_values: List()
+
+    :return:
+        the covariance as defined
+    """
+
+    # Avrages of the lists
+    x_avg = mean(first_list_of_values)
+    y_avg = mean(second_list_of_values)
+
+    # Subtract the respective avg from each value in the values
+    first_diff_from_avg = sub_num_from_iterable(
+        x_avg, first_list_of_values)
+    scnd_diff_from_avg = sub_num_from_iterable(
+        y_avg, second_list_of_values)
+
+    # Compute the nominator of the covariance as defined
+    nominator = sum(
+        [x * y for x, y in zip(first_diff_from_avg, scnd_diff_from_avg)]
+    )
+
+    # Returns the convariance as defined
+    return nominator / (len(first_diff_from_avg) - 1)
 
 
 def correlation(first_list_of_values, second_list_of_values):
-    result = 0
-    # Place your code here
-    return result
+    """
+    :arg:
+        :param first_list_of_values: the first list of data
+        :type first_list_of_values: List()
+        :param second_list_of_values: the second list of data
+        :type second_list_of_values: List()
 
+    :return:
+        the correlation as defined
+    """
+    # Calls the function standard_deviation
+    # to compute the standard deviation
+    std_dev1 = standard_deviation(first_list_of_values)
+    std_dev2 = standard_deviation(second_list_of_values)
+
+    # Return -2 if correlation is not be defined
+    # Makes sure not to divide by 0
+    if(std_dev1 == 0 or std_dev2 == 0):
+        return -2
+
+    # Computes the correlation as its definition
+    denominator = std_dev1 * std_dev2
+    cov = covariance(first_list_of_values, second_list_of_values)
+
+    return (cov / denominator)
