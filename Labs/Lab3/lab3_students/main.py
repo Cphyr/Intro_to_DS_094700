426c592d6e9e555a47c015091bd11905235d018d
OriMeiraz <ori.meiraz@campus.technion.ac.il>
2021-05-05 23:22:02 +0300

    finished - ori

--- Labs/Lab3/lab3_students/main.py
+++ Labs/Lab3/lab3_students/main.py
@@ -20,16 +20,13 @@ def load_data(input_path):
 
 
 def run_kmeans():
-    if len(argv) < 4:
-        print('Not enough arguments provided. Please provide 3 arguments: K, num_iterations, path_to_input')
+    if len(argv) < 5:
+        print('Not enough arguments provided. Please provide 4 arguments: K, num_iterations, path_to_input, seed')
         exit(1)
     k = int(argv[1])
     num_iterations = int(argv[2])
     input_path = argv[3]
-    if len(argv) == 5:
-        random_seed = int(argv[4])
-    else:
-        random_seed = 0
+    random_seed = int(argv[4])
 
     if k <= 1 or num_iterations <= 0:
         print('Please provide correct parameters')
@@ -45,9 +42,9 @@ def run_kmeans():
 
     runner = KMeans(k, num_iterations)
     runner.run(points, random_seed)
-    print("seed K SL1")
     runner.print_results(random_seed)
 
 
 if __name__ == '__main__':
+    # Expected input: argv[1:] = [K, num_iterations, path_to_input, seed]
     run_kmeans()
