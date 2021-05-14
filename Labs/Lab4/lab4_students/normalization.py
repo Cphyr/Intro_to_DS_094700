da76e32bb8e0378750474e65789febff184aa976
OriMeiraz <81318835+OriMeiraz@users.noreply.github.com>
2021-05-14 12:48:38 +0300

    docs MinMax

--- Labs/Lab4/lab4_students/normalization.py
+++ Labs/Lab4/lab4_students/normalization.py
@@ -38,17 +38,24 @@ class MinMaxNormalizer:
         pass
 
     def transform(self, points):
-        min_dic = {}
-        max_dic = {}
-        new_points = []
-        all_coordinates = [p.coordinates for p in points]
+        """"
+        Params: 
+            self: the MinMaxNormalizer object
+            points: Iterable of Point objects
+        Returns: 
+            new_points: Iterable of Point objects. The coordinates of each points is the MinMax    
+        """
+        min_dic = {} #dictionary of the min in the certain dimension
+        max_dic = {} #dictionary of the max in the certain dimension
+        new_points =  #creates the list for the new points
+        all_coordinates = [p.coordinates for p in points] #creates list of all coordinates
         dimensions = len(all_coordinates[0])
-        for i in range(dimensions):
+        for i in range(dimensions): #finds out the min and max in all of the dimensions
             values = [x[i] for x in all_coordinates]
             min_dic[i] = min(values)
             max_dic[i] = max(values)
 
-        for i, p in enumerate(points):
+        for i, p in enumerate(points): #calculates the min-max transformation and adds the new point to the list
             current = p.coordinates
             new_points.append(Point(p.name, [(current[i]-min_dic[i])/(max_dic[i]-min_dic[i])
                                              for i in range(dimensions)], p.label))
