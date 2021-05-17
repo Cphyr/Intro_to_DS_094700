4573c99f0760e0353409f2a470c82e42c4b3ca01
OriMeiraz <ori.meiraz@campus.technion.ac.il>
2021-05-17 22:32:01 +0300

    Cluster's merge and docs

--- Ex3/cluser.py
+++ Ex3/cluser.py
@@ -4,4 +4,15 @@ class Cluster:
         self.samples = samples
 
     def merge(self, other):
-        pass
+        """
+        Params:
+            self: this Cluster object
+            other: another Cluster object
+        """
+        # Changes the c_id to the minimum between the two c_ids
+        self.c_id = min(
+            self.c_id, other.c_id)
+        self.samples = self.samples+other.samples  # Adds the lists together
+        # Sorts the list by the s_id of the Samples
+        self.samples.sort(key=lambda sample: sample.s_id)
+        del other  # Deletes the other cluster
