28c005939e2311b20ecf7d95b451d012785cb887
OriMeiraz <81318835+OriMeiraz@users.noreply.github.com>
2021-05-26 15:00:45 +0300

    main docs

--- Ex3/main.py
+++ Ex3/main.py
@@ -8,11 +8,13 @@ from agglomerative_clustering import Agglomerative_Cluster
 def main(argv):
     #data = Data(argv[1])
     data = Data("Leukemia_sample.csv")
+    # Creates a single link and complete link object
     single = SingleLink()
     complete = CompleteLink()
     max_clus = 7
 
     for link in [single, complete]:
+        # Loops through both of the links and runs agglomerative clustering
         agg_clus = Agglomerative_Cluster(link, data.create_samples())
         agg_clus.run(max_clus)
         if link == single:
